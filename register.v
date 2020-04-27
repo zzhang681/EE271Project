867cb9817bfb76915fae8539b81973f1e4db913f
cruisers <89080385@qq.com>
2020-04-26 23:04:12 -0700

    Update register.v

--- register.v
+++ register.v
@@ -1,13 +1,26 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 //
-
+//                       16|<-Din
+//                         |
+//                 ------------------
+//                 |               <|---CLK
+//             3---|num_R1          |
+//             3---|num_R2          |
+//                 |        RF      |
+//             1---|WE              |
+//             3---|W1              |
+//                 ------------------
+//                     |16      |16 
+//                     |        |
+//                   Dout_1   Dout_2    (will pass to ALU)
+//////////////////////////////////////////////////////////////////////////////////
 
 module register(CLK, num_R1, num_R2, W1, Din, WE, Dout_1, Dout_2
     
     );
     input CLK, WE;  //clk, write_enable_signal
-    input[2:0] num_R1, num_R2;
+    input[2:0] num_R1, num_R2;  //Read command, read RF[specific position]
     input[2:0] W1;  //reg # to write to
     input[15:0] Din;
     output reg[15:0] Dout_1, Dout_2;
