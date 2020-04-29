753c3d94e9d5b3d00893de0fd1b9cfb6922d942e
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-04-29 11:27:34 -0700

    topLevel module is done

--- verilog/topLevel.sv
+++ verilog/topLevel.sv
@@ -6,4 +6,8 @@ module topLevel(clk, clear, next, MS, Din, Done_out, CS_out, Alu_out);
 	output [2:0] CS_out;
 	output [15:0] Alu_out;
 
-	controll
+	wire [2:0] MS_wire;
+	wire W1_wire, WE_wire;
+
+	FSM sfm1 (clk, clear, next, MS, MS_wire, CS_out, Done_out, W1_wire, WE_wire);
+	Datapath datapath1 (clk, Din, WE_wire, W1_wire, MS_wire, Alu_out);
