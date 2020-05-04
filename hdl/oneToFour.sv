a6bb476372b59f2afec6a380ddb7aaff73b2c601
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-05-04 13:17:05 -0700

    Create oneToFour.sv

---
+++ hdl/oneToFour.sv
@@ -0,0 +1,10 @@
+module oneToFour(in, out3, out2, out1, out0);
+	input [15:0] in;
+	output [3:0] out3, out2, out1, out0;
+
+	assign out3 = in[15:12];
+	assign out2 = in[11:8];
+	assign out1 = in[7:4];
+	assign out0 = in[3:0];
+	
+endmodule
