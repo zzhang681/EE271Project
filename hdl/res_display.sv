964dfaea94e149b62c645293cf25544bb01d9133
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-05-04 18:28:57 -0700

    update

--- hdl/res_display.sv
+++ hdl/res_display.sv
@@ -1,12 +1,12 @@
-module res_display(in1, in2, out3, out2, out1, out0);
-  input[7:0] in1, in2;
+module res_display(in, out3, out2, out1, out0);
+  input[15:0] in;
   output[3:0] out3, out2, out1, out0;
-  wire [7:0] val1, val2, val3;
+  wire [15:0] val1, val2, val3;
   
   
 
-  assign out0 = in1 % 10;
-  assign val1 = in1 / 10;
+  assign out0 = in % 10;
+  assign val1 = in / 10;
   
   assign out1 = val1 % 10;
   assign val2 = val1 / 10;
@@ -15,8 +15,6 @@ module res_display(in1, in2, out3, out2, out1, out0);
   assign val3 = val2 / 10;
   
   assign out3 = val3 % 10;
-  
-  
   
   
 endmodule
