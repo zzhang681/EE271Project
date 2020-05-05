13777e4fc650ca6c2e9608af4a55ceff20635a00
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-05-04 18:05:51 -0700

    finalized

--- hdl/res_display.sv
+++ hdl/res_display.sv
@@ -1,4 +1,4 @@
-module design1(in1, in2, out3, out2, out1, out0);
+module res_display(in1, in2, out3, out2, out1, out0);
   input[7:0] in1, in2;
   output[3:0] out3, out2, out1, out0;
   wire [7:0] val1, val2, val3;
